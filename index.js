413765babc0f0bbafffb704678c3d36b623876b9
gorushkin <gorushkin@gmail.com>
2019-12-05 17:12:49 +0300

    home

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const print = str => console.log(str);
 const fs = require('fs');
+const time = new Date();
 // const map = require('async');
 
 const file = './file';
-const txt = 'qwerty';
+const txt = time;
 
-fs.unlinkSync(file);
+// fs.unlinkSync(file);
+const cb = (err, msg) => print(msg);
 
 const writeToFile = (dest, txt) => {
   fs.writeFile(dest, txt, (err) => {
     if (err) {
-      print(err);
+      cb(err, null);
       return
     }
-    print('job is done');
+    cb(null, 'job is done');
   });
 }
 
+// const writeToFile = (dest, txt) => {
+//   fs.writeFile(dest, txt, cb);
+// }
+
+const readFile =  (file) => {
+  fs.readFile(file, 'utf-8', (error, data) => {
+    if (error) {
+      cb(error, null);
+      return
+    }
+    cb(null, data);
+  });
+};
+
 // writeToFile(file, txt);
 print('stop')
+readFile(file);
